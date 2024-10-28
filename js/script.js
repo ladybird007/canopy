f6e678db3a1165955c80789cb96c01133be730f3
ishunter216 <michael.king@edenprogram.com>
2024-10-28 19:26:01 +0200

    phone number script

--- js/script.js
+++ js/script.js
@@ -90,15 +90,38 @@
     });
 
     // Input 'tel' validate
-    function validatePhoneNumber(input) {
-      //let re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/g;
-      console.log(input.val().replace(/^[^3]+|^([0-9]{10}).*|[^0-9]+/g, '$1'));
-      
+    function addDashes(f)
+    {
+      console.log(f.val());
+      let f_val = f.val();
+      return "("+f_val.substr(0,3)+")"+f_val.substr(3,3)+"-"+f_val.substr(3,4);
     }
 
-    $('#phone-number').on('keyup', function() {
-      validatePhoneNumber($(this));
-    });   
+    // $('#phone-number').on('keydown', function() {
+    //   //$(this).val(addDashes($(this)));
+    //   $(this).val($(this).val().replace(/(\d{3})\-?(\d{3})\-?(\d{4})/,'$1-$2-$3'))
+    // });   
+    
+    $('#phone-number').on('keydown', function(e) {
+      var output,
+        $this = $(this),
+        input = $this.val();
+  
+      if(e.keyCode != 8) {
+        input = input.replace(/[^0-9]/g, '');
+        var area = input.substr(0, 3);
+        var pre = input.substr(3, 3);
+        var tel = input.substr(6, 4);
+        if (area.length < 3) {
+          output = "(" + area;
+        } else if (area.length == 3 && pre.length < 3) {
+          output = "(" + area + ")" + " " + pre;
+        } else if (area.length == 3 && pre.length == 3) {
+          output = "(" + area + ")" + " " + pre + "-" + tel;
+        }
+        $this.val(output);
+      }
+    });
 
   })
 })(jQuery)
